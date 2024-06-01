e91fdd23148a882aac40d2e6c2c3f7134e786123
stefhannyMSB <154107603+stefhannyMSB@users.noreply.github.com>
2024-06-01 19:11:28 +0700

    matapelajaran
    matapelajaran

--- index.blade.php
+++ index.blade.php
@@ -1,70 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row mx-3 my-1">
-    <h2 class="text-dark">DATA KELAS</h2>
-</div>
-{{-- alert --}}
-@if (Session::has('success'))
-<div id="successAlert" class="alert alert-success" role="alert">
+    <div class="row mx-3 my-1">
+        <h2 class="text-dark">DATA MATA PELAJARAN</h2>
+    </div>
+    {{-- alert --}}
+    @if (Session::has('success'))
+        <div id="successAlert" class="alert alert-success" role="alert">
 
-    <button id="closeButton" class="close-button" onclick="closeAlert()">&times;</button>
-</div>
-@endif
+            <button id="closeButton" class="close-button" onclick="closeAlert()">&times;</button>
+        </div>
+    @endif
 
-<script>
-    function closeAlert() {
-        document.getElementById('successAlert').style.display = 'none';
-    }
-</script>
-<div class="row">
-    <div class="col-md">
-        <a href="{{ route('kelas.create') }}" class="btn btn-dark btn-sm mb-2 mx-3"><i class="fa fa-users" aria-hidden="true"></i>&nbsp;Tambah Data</a>
-        <a href="#" class="btn btn-success btn-sm mb-2"><i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak
-            Data</a>
-        <div class="card shadow py-2 px-2">
-            <div class="card-header my-1 mx-1">
-                <table id="example" class="table table-striped" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Kelas</th>
-                            <th scope="col">Jurusan</th>
-                            <th scope="col">Walikelas</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($kelas as $kelas)
-                        <tr>
-                            <td class="align-middle">{{ $loop->index + 1 }}</td>
-                            <td class="align-middle">{{ $kelas->kelas }}</td>
-                            <td class="align-middle">{{ $kelas->jurusan['jurusan']}}</td>
-                            <td class="align-middle">{{ $kelas->guru['nama']}}</td>
-                            <td class="align-middle">
-                                {{-- <div class="btn-group" role="group" aria-label="Basic example">
+    <script>
+        function closeAlert() {
+            document.getElementById('successAlert').style.display = 'none';
+        }
+    </script>
+    <div class="row">
+        <div class="col-md">
+            <a href="{{ route('matapelajaran.create') }}" class="btn btn-dark btn-sm mb-2 mx-3"><i
+                    class="fa fa-users"aria-hidden="true"></i>&nbsp;Tambah Data</a>
+            <a href="#" class="btn btn-success btn-sm mb-2"><i class="fa fa-print" aria-hidden="true"></i>&nbsp;Cetak
+                Data</a>
+            <div class="card shadow py-2 px-2">
+                <div class="card-header my-1 mx-1">
+                    <table id="example" class="table table-striped" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Mata Pelajaran</th>
+                                <th scope="col">Jenis Pembelajaran</th>
+                                <th scope="col">Jurusan</th>
+                                <th scope="col">Guru Pengajar</th>
+                                <th scope="col">Tahun Akademik</th>
+                                <th scope="col">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($matapelajaran as $matapelajaran)
+                                <tr>
+                                    <td class="align-middle">{{ $loop->index + 1 }}</td>
+                                    <td class="align-middle">{{ $matapelajaran->pelajaran }}</td>
+                                    <td class="align-middle">{{ $matapelajaran->pembelajaran }}</td>
+                                    <td class="align-middle">{{ $matapelajaran->jurusan['jurusan']}}</td>
+                                    <td class="align-middle">{{ $matapelajaran->guru['nama']}}</td>
+                                    <td class="align-middle">{{ $matapelajaran->tahun }}</td>
+                                    <td class="align-middle">
+                                        {{-- <div class="btn-group" role="group" aria-label="Basic example">
                                             <a href="{{route(mata_pelajaran.edit)}}" type="button" class="btn btn-warning">Update</a>
-                                <form action="{{ route('mata_pelajaran.destroy', $mata_pelajaran->id_mata_pelajaran) }}" method="POST" class="btn btn-danger" onsubmit="return confirm('Apakah Anda yakin ingin menghapus ini?')">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger p-0 mb-1">Delete</button>
-                                </form>
-            </div> --}}
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <a href="{{ route('kelas.edit', $kelas->id_kelas) }}" type="button" class="btn btn-warning">Update</a>
-                <form action="{{ route('kelas.destroy', $kelas->id_kelas) }}" method="POST" class="btn btn-danger" onsubmit="return confirm('Apakah Anda yakin ingin menghapus ini?')">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger p-0 mb-1">Delete</button>
-                </form>
+                                            <form action="{{ route('mata_pelajaran.destroy', $mata_pelajaran->id_mata_pelajaran) }}" method="POST"
+                                                class="btn btn-danger"
+                                                onsubmit="return confirm('Apakah Anda yakin ingin menghapus ini?')">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger p-0 mb-1">Delete</button>
+                                            </form>
+                                        </div> --}}
+                                        <div class="btn-group" role="group" aria-label="Basic example">
+                                            <a href="{{ route('matapelajaran.edit', $matapelajaran->id_matapelajaran) }}" type="button"
+                                                class="btn btn-warning">Update</a>
+                                            <form action="{{ route('matapelajaran.destroy', $matapelajaran->id_matapelajaran) }}" method="POST">
+                                                {{-- cross site ruquest forgery memverifikasi bahwa pengguna yang membuat permintaan --}}
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit" class="btn btn-danger"
+                                                    onclick="return confirm('Yakin Ingin Dihapus?')">Delete</button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            </td>
-            </td>
-            </tr>
-            @endforeach
-            </tbody>
-            </table>
         </div>
     </div>
-</div>
-</div>
 @endsection
