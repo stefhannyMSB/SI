1c24de56d8078a92947c4ee419f40464f26b6cf1
stefhannyMSB <154107603+stefhannyMSB@users.noreply.github.com>
2024-06-01 19:12:54 +0700

    murid
    murid

--- report.blade.php
+++ report.blade.php
@@ -12,38 +12,43 @@
             border: 1px;
         }
     </style>
-    <title>Cetak Jadwal Pelajaran</title>
+    <title>Cetak Data Siswa</title>
 </head>
 
 <body>
     <div class="form-group">
-    <h1 align="center"><b>Laporan Data Jadwal Pelajaran</b></h1>
-    <p  align="center"><b>SI akademik</b></p>
-    <table class="static" align="center" rules="all" border="1px" style="width: 95%;">
-        <tr>
-            <th scope="col">No</th>
-            <th scope="col">Hari</th>
-            <th scope="col">Waktu</th>
-            <th scope="col">Jurusan</th>
-            <th scope="col">Kelas</th>
-            <th scope="col">Mata Pelajaran</th>
-        </tr>
-        @foreach ($jadwal as $jadwal)
+        <p align="center"><b>Laporan Data Siswa</b></p>
+        <p align="center"><b>SI akademik</b></p>
+        <table class="static" align="center" rules="all" border="1px" style="width: 95%;">
             <tr>
-                <td class="align-middle">{{ $loop->index + 1 }}</td>
-                <td class="align-middle">{{ $jadwal->hari }}</td>
-                <td class="align-middle">{{ $jadwal->waktu }}</td>
-                <td class="align-middle">{{ $jadwal->jurusan['jurusan'] }}</td>
-                <td class="align-middle">{{ $jadwal->kelas['kelas'] }}</td>
-                <td class="align-middle">{{ $jadwal->matapelajaran['pelajaran'] }}</td>
+                <th scope="col">No</th>
+                <th scope="col">NIS</th>
+                <th scope="col">Nama Murid</th>
+                <th scope="col">Tempat Lahir</th>
+                <th scope="col">Tanggal Lahir</th>
+                <th scope="col">Jenis Kelamin</th>
+                <th scope="col">Agama</th>
+                <th scope="col">Jurusan</th>
+                <th scope="col">Ekstrakurikuler</th>
             </tr>
-        @endforeach
-    </table>
+            @foreach ($murid as $murid)
+                <tr>
+                    <td class="align-middle">{{ $loop->index + 1 }}</td>
+                    <td class="align-middle">{{ $murid->NIS }}</td>
+                    <td class="align-middle">{{ $murid->nama }}</td>
+                    <td class="align-middle">{{ $murid->tempat_lahir }}</td>
+                    <td class="align-middle">{{ $murid->tgl_lahir }}</td>
+                    <td class="align-middle">{{ $murid->jenis_kelamin }}</td>
+                    <td class="align-middle">{{ $murid->agama }}</td>
+                    <td class="align-middle">{{ $murid->jurusan['jurusan'] }}</td>
+                    <td class="align-middle">{{ $murid->ekstrakurikuler }}</td>
+                </tr>
+            @endforeach
+        </table>
     </div>
     <script type="text/javascript">
         window.print();
     </script>
-</body>
 </body>
 
 </html>
